31ca8236f5e7cce55247780a02d9c0346047586a
WPY <WPY@DESKTOP-FS939E1.fios-router.home>
2017-06-02 22:16:59 -0400

    add xbox controller input capture

--- self_drive/create_training_data.py
+++ self_drive/create_training_data.py
@@ -6,8 +6,11 @@ import cv2
 import time
 from getkeys import key_check
 import os
+import threading
 from basic_setup import WIDTH, HEIGHT, REGION, pre_process_image
+import xbox_input
 
+x_value = float(0)
 
 def keys_to_output(keys):
     '''
@@ -33,10 +36,44 @@ if os.path.isfile(file_name):
 else:
     print('File does not exist, starting fresh!')
     training_data = []
+    
+def captureX():
+    """
+    Grab 1st available gamepad, logging changes to the screen.
+    L & R analogue triggers set the vibration motor speed.
+    """
+    joysticks = xbox_input.XInputJoystick.enumerate_devices()
+    device_numbers = list(map(xbox_input.attrgetter('device_number'), joysticks))
+
+    print('found %d devices: %s' % (len(joysticks), device_numbers))
+
+    if not joysticks:
+        sys.exit(0)
+
+    j = joysticks[0]
+    print('using %d' % j.device_number)
+    
+    @j.event
+    def on_axis(axis, value):
+        global x_value 
+        if axis == "l_thumb_x":
+            x_value = value
+            #print('X', type(value), x_value)
+
+    while True:
+        j.dispatch_events()
+        time.sleep(.01)
 
 
 if __name__ == '__main__':
 
+    ''' This section starts a thread and capture xbox controller input, use only when xbox controller is connected '''
+    global x_value
+    t = threading.Thread(target=captureX)
+    t.daemon = True
+    t.start()
+    ''' xbox end '''
+    
     print("Get ready: ")
     for i in list(range(4))[::-1]:
         print(i+1)
@@ -49,10 +86,16 @@ if __name__ == '__main__':
             
             screen = grab_screen(region=REGION)
             screen = pre_process_image(screen)
-
+#             print(screen)
             # resize to something a bit more acceptable for a CNN
-            keys = key_check()
-            output = keys_to_output(keys)
+            
+            ''' Capture keyboard and use one-hot vector as output (classification) '''
+            #keys = key_check()
+            #output = keys_to_output(keys)
+            
+            ''' Capture xbox controller and use short variable as output (regression) '''
+            output = x_value
+            
             print(output)
             training_data.append([screen,output])
             
